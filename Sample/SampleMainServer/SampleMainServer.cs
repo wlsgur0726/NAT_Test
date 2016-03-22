b6dabba6aec06ad9c5d10f34531e3a2ca4bcd24a
wlsgur0726 <wlsgur0726@naver.com>
2016-03-23 01:56:28 +0900

    샘플 서버들 인자 설명 추가

--- Sample/SampleMainServer/SampleMainServer.cs
+++ Sample/SampleMainServer/SampleMainServer.cs
@@ -8,6 +8,7 @@ namespace SampleMainServer
 	{
 		static void Main(string[] args)
 		{
+			bool invalidParameter = true;
 			MainServer mainServer = null;
 			try {
 				if (args.Length < 6)
@@ -19,6 +20,8 @@ namespace SampleMainServer
 											Common.ParseURL(args[3]),
 											Common.ParseURL(args[4]),
 											Common.ParseURL(args[5]));
+
+				invalidParameter = false;
 				mainServer.Start();
 				Console.WriteLine("MainServer 시작");
 				while (Console.ReadKey().Key != ConsoleKey.Escape) {
@@ -26,6 +29,15 @@ namespace SampleMainServer
 			}
 			catch (Exception e) {
 				Console.Error.WriteLine(e.ToString());
+				if (invalidParameter) {
+					Console.Error.WriteLine("");
+					Console.Error.WriteLine("  1번째 인자  :  이 서버의 TCP 바인딩 주소1 (ex 0.0.0.0:10001)");
+					Console.Error.WriteLine("  2번째 인자  :  이 서버의 TCP 바인딩 주소2 (ex 0.0.0.0:10002)");
+					Console.Error.WriteLine("  3번째 인자  :  SubServer의 TCP 주소 (ex 11.22.33.44:11111)");
+					Console.Error.WriteLine("  4번째 인자  :  이 서버의 UDP 바인딩 주소1 (ex 0.0.0.0:20001)");
+					Console.Error.WriteLine("  5번째 인자  :  이 서버의 UDP 바인딩 주소2 (ex 0.0.0.0:20002)");
+					Console.Error.WriteLine("  6번째 인자  :  SubServer의 UDP 주소 (ex 11.22.33.44:22222)");
+				}
 			}
 			finally {
 				Console.WriteLine("종료");
--- Sample/SampleSubServer/SampleSubServer.cs
+++ Sample/SampleSubServer/SampleSubServer.cs
@@ -8,13 +8,15 @@ namespace SampleSubServer
 	{
 		static void Main(string[] args)
 		{
+			bool invalidParameter = true;
 			SubServer subServer = null;
 			try {
 				if (args.Length < 2)
 					throw new ArgumentException("IP:Port 정보들을 인자로 넘겨주세요.");
-
 				subServer = new SubServer(Common.ParseURL(args[0]),
 										  Common.ParseURL(args[1]));
+
+				invalidParameter = false;
 				subServer.Start();
 				Console.WriteLine("SubServer 시작");
 				while (Console.ReadKey().Key != ConsoleKey.Escape) {
@@ -22,6 +24,11 @@ namespace SampleSubServer
 			}
 			catch (Exception e) {
 				Console.Error.WriteLine(e.ToString());
+				if (invalidParameter) {
+					Console.Error.WriteLine("");
+					Console.Error.WriteLine("  1번째 인자  :  이 서버의 TCP 바인딩 주소 (ex 0.0.0.0:11111)");
+					Console.Error.WriteLine("  2번째 인자  :  이 서버의 UDP 바인딩 주소 (ex 0.0.0.0:22222)");
+				}
 			}
 			finally {
 				Console.WriteLine("종료");
