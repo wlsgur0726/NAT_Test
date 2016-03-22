2083500c4dfac66d000b2ffcc82a49c78f7c5a4b
wlsgur0726 <wlsgur0726@naver.com>
2016-03-23 02:03:51 +0900

    샘플서버들 인자 설명 보강

--- Sample/SampleMainServer/SampleMainServer.cs
+++ Sample/SampleMainServer/SampleMainServer.cs
@@ -31,12 +31,16 @@ namespace SampleMainServer
 				Console.Error.WriteLine(e.ToString());
 				if (invalidParameter) {
 					Console.Error.WriteLine("");
+					Console.Error.WriteLine("인자 설명");
 					Console.Error.WriteLine("  1번째 인자  :  이 서버의 TCP 바인딩 주소1 (ex 0.0.0.0:10001)");
 					Console.Error.WriteLine("  2번째 인자  :  이 서버의 TCP 바인딩 주소2 (ex 0.0.0.0:10002)");
 					Console.Error.WriteLine("  3번째 인자  :  SubServer의 TCP 주소 (ex 11.22.33.44:11111)");
 					Console.Error.WriteLine("  4번째 인자  :  이 서버의 UDP 바인딩 주소1 (ex 0.0.0.0:20001)");
 					Console.Error.WriteLine("  5번째 인자  :  이 서버의 UDP 바인딩 주소2 (ex 0.0.0.0:20002)");
 					Console.Error.WriteLine("  6번째 인자  :  SubServer의 UDP 주소 (ex 11.22.33.44:22222)");
+					Console.Error.WriteLine("");
+					Console.Error.WriteLine("예제");
+					Console.Error.WriteLine("  SampleMainServer.exe 0.0.0.0:10001 0.0.0.0:10002 11.22.33.44:11111 0.0.0.0:20001 0.0.0.0:20002 11.22.33.44:22222");
 				}
 			}
 			finally {
--- Sample/SampleSubServer/SampleSubServer.cs
+++ Sample/SampleSubServer/SampleSubServer.cs
@@ -26,8 +26,12 @@ namespace SampleSubServer
 				Console.Error.WriteLine(e.ToString());
 				if (invalidParameter) {
 					Console.Error.WriteLine("");
+					Console.Error.WriteLine("인자 설명");
 					Console.Error.WriteLine("  1번째 인자  :  이 서버의 TCP 바인딩 주소 (ex 0.0.0.0:11111)");
 					Console.Error.WriteLine("  2번째 인자  :  이 서버의 UDP 바인딩 주소 (ex 0.0.0.0:22222)");
+					Console.Error.WriteLine("");
+					Console.Error.WriteLine("예제");
+					Console.Error.WriteLine("  SampleSubServer.exe 0.0.0.0:11111 0.0.0.0:22222");
 				}
 			}
 			finally {
